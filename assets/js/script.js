6bb4e7391d3837d511cef25897f081870ebd5343
Denis McDonald <denis.l.mcdonald@gmail.com>
2019-03-26 10:21:05 +1100

    Add theme persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,36 @@ var links = document.getElementsByTagName("a");
 var lightSwitch = document.querySelector(".light-switch");
 var lightSrc = lightSwitch.getAttribute("src");
 
-lightSwitch.addEventListener("click", function changeTheme() {
+if (localStorage.getItem("themePreference") == "1") {
  
-    if (lightSrc == "/assets/images/light-on.svg") {
-        lightSwitch.setAttribute("src", "/assets/images/light-off.svg");
-        body.classList.toggle("body-dark");
+  lightSwitch.setAttribute("src", "/assets/images/light-on.svg");
+        lightSwitch.setAttribute("title", "Turn on dark colour theme");
+        body.classList.toggle("body-light");
         for (i = 0; i < links.length; i++) {
-            links[i].classList.add("a-dark");
+            links[i].classList.add("a-light");
           }
         lightSrc = lightSwitch.getAttribute("src");
-    } else if (lightSrc == "/assets/images/light-off.svg") {
+
+};
+
+lightSwitch.addEventListener("click", function changeTheme() {
+    if (lightSrc == "/assets/images/light-off.svg") {
         lightSwitch.setAttribute("src", "/assets/images/light-on.svg");
-        body.classList.toggle("body-dark");
+        lightSwitch.setAttribute("title", "Turn on dark colour theme");
+        body.classList.toggle("body-light");
         for (i = 0; i < links.length; i++) {
-            links[i].classList.remove("a-dark");
+            links[i].classList.add("a-light");
           }
         lightSrc = lightSwitch.getAttribute("src");
+        localStorage.setItem("themePreference", "1");
+    } else {
+        lightSwitch.setAttribute("src", "/assets/images/light-off.svg");
+        body.classList.toggle("body-light");
+        lightSwitch.setAttribute("title", "Turn on light colour theme");
+        for (i = 0; i < links.length; i++) {
+            links[i].classList.remove("a-light");
+          }
+        lightSrc = lightSwitch.getAttribute("src");
+        localStorage.setItem("themePreference", "0");
     } 
 });
-
-
-
-
