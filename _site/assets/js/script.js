f9546de48700b7507f3106f8799f4683c562753e
Denis McDonald <denis.l.mcdonald@gmail.com>
2019-04-05 10:42:12 +1100

    D logo

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -12,11 +12,12 @@ if (localStorage.getItem("themePreference") === "1") {
 
 // Apply light theme and set user preference to light theme
 function lightsOn() {
-  var lightSwitch = document.querySelector(".light-switch");
+  // var lightSwitch = document.querySelector(".light-switch");
+  var lightSwitch = document.querySelector(".toggle-theme");
   localStorage.setItem("themePreference", "1");
   lightSwitch.removeEventListener("click", lightsOn);
   lightSwitch.addEventListener("click", lightsOff);
-  lightSwitch.style.backgroundImage = "url('assets/images/light-on.svg')";
+  // lightSwitch.style.backgroundImage = "url($baseurl + '/assets/images/light-off.svg')";
   document.body.classList.add("body-light");
   for (i = 0; i < document.getElementsByTagName("a").length; i++) {
     document.getElementsByTagName("a")[i].classList.add("a-light");
@@ -28,11 +29,13 @@ function lightsOn() {
 
 // Apply dark theme and set user preference to light theme
 function lightsOff() {
-  var lightSwitch = document.querySelector(".light-switch");
+  // var lightSwitch = document.querySelector(".light-switch");
+  var lightSwitch = document.querySelector(".toggle-theme");
   localStorage.setItem("themePreference", "0");
   lightSwitch.removeEventListener("click", lightsOff);
   lightSwitch.addEventListener("click", lightsOn);
-  lightSwitch.style.backgroundImage = "url('assets/images/light-off.svg')";
+  // lightSwitch.style.backgroundImage = "url('assets/images/light-off.svg')";
+  // lightSwitch.style.backgroundImage = "url($baseurl + '/assets/images/light-off.svg')";
   document.body.classList.remove("body-light");
   for (i = 0; i < document.getElementsByTagName("a").length; i++) {
     document.getElementsByTagName("a")[i].classList.remove("a-light");
